ca4f82c2029ae468c2aa05a8b95a01060bbebff6
minnjung0887921 <minjeong0887921@gmail.com>
2024-08-26 13:01:59 +0900

    [컬렉션 조회 최적화] 주문 조회 V1 : 엔티티 직접 노출

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -23,7 +23,7 @@ public class OrderItem {
     @JoinColumn(name = "item_id")
     private Item item;;
 
-//    @JsonIgnore
+    @JsonIgnore
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
